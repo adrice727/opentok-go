ed04b41c6357756101b95d3fd1f5c71cd9c2cf5a
Aaron Rice <adrice727@gmail.com>
2016-08-23 13:19:16 -0700

    Update tests for archiving.

--- opentok_test.go
+++ opentok_test.go
@@ -26,14 +26,35 @@ func TestConfiguration(t *testing.T) {
 
 func TestSessionCreation(t *testing.T) {
 	ot := Opentok{config.APIKey, config.APISecret}
-	session := ot.CreateSession()
+	session, _ := ot.CreateSession()
 	fmt.Printf("%+v\n", session)
 }
 
 func TestTokenCreation(t *testing.T) {
 	ot := Opentok{config.APIKey, config.APISecret}
-	session := ot.CreateSession()
+	session, _ := ot.CreateSession()
 	token := ot.GenerateToken(session.SessionID)
 	fmt.Println("session", session)
 	fmt.Printf("%+v\n", token)
+}
+
+// There must be an active publisher in order to start archiving
+func TestStartArchive(t *testing.T) {
+	ot := Opentok{config.APIKey, config.APISecret}
+	session, _ := ot.CreateSession()
+	archive, _ := ot.StartArchive(session.SessionID)
+	fmt.Printf("Archive created for session %s", session.SessionID)
+	fmt.Print(archive)
+}
+
+// An active session
+func TestStopArchive(t *testing.T) {
+	ot := Opentok{config.APIKey, config.APISecret}
+	archiveID := ""
+	err := ot.StopArchive(archiveID)
+	if err != nil {
+		fmt.Println("Error stopping the archive")
+	} else {
+		fmt.Println("Succesfully stopped the archive")
+	}
 }
